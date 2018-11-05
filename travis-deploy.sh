a4d41730e22f7f062999ae975ee9255e74218826
Philipp Jardas <philipp@jardas.de>
2018-11-05 21:25:21 +0100

    add travis deployment

---
+++ travis-deploy.sh
@@ -0,0 +1,7 @@
+#!/bin/bash -e
+if [ "$TRAVIS_BRANCH" == "master" -a "$TRAVIS_PULL_REQUEST" == "false" ]; then
+  echo "Deploying branch $TRAVIS_BRANCH to Firebase"
+  npm run deploy -- --token=${FIREBASE_TOKEN}
+else
+  echo "Skipping Firebase deployment of branch $TRAVIS_BRANCH"
+fi
