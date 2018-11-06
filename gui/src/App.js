513998fa198d334415455f46d9aacc225656e228
Philipp Jardas <philipp@jardas.de>
2018-11-06 18:24:27 +0100

    Animate route transitions

--- gui/src/App.js
+++ gui/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Redirect, Switch } from 'react-router-dom';
 import Route from 'react-router-dom/Route';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
 import ScrollToTop from './components/ScrollToTop';
 import Song from './pages/Song';
 import Songs from './pages/Songs';
@@ -10,13 +11,21 @@ export default function App() {
   return (
     <Router>
       <ScrollToTop>
-        <Switch>
-          <Route path="/songs/:songId" component={Song} />
-          <Route path="/songs" component={Songs} />
-          <Route exact path="/">
-            {() => <Redirect to="/songs" />}
-          </Route>
-        </Switch>
+        <Route>
+          {({ location }) => (
+            <TransitionGroup component={null}>
+              <CSSTransition key={location.key} classNames="fade" timeout={300}>
+                <Switch location={location}>
+                  <Route path="/songs/:songId" component={Song} />
+                  <Route path="/songs" component={Songs} />
+                  <Route exact path="/">
+                    {() => <Redirect to="/songs" />}
+                  </Route>
+                </Switch>
+              </CSSTransition>
+            </TransitionGroup>
+          )}
+        </Route>
       </ScrollToTop>
     </Router>
   );
--- gui/src/components/Layout.js
+++ gui/src/components/Layout.js
@@ -4,21 +4,21 @@ import { NavLink as RRLink } from 'react-router-dom';
 import { Container, Navbar, NavbarBrand } from 'reactstrap';
 import FontAwesome from './FontAwesome';
 
-export default function Layout({ title = 'Songbook', icon = 'music', children }) {
+export default function Layout({ title, icon = 'music', children }) {
   return (
-    <>
-      <Helmet>
+    <div>
+      <Helmet titleTemplate="%s - Songbook" defaultTitle="Songbook">
         <title>{title}</title>
       </Helmet>
       <Navbar color="primary" dark fixed="top" className="navbar-expand">
         <Container>
           <NavbarBrand to="/" tag={RRLink}>
             <FontAwesome icon={icon} className="ml-3 mr-2" />
-            {title}
+            {title || 'Songbook'}
           </NavbarBrand>
         </Container>
       </Navbar>
       {children}
-    </>
+    </div>
   );
 }
