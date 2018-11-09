94b177b9fd0a272c20181baa3d3d77f606a78bd3
Philipp Jardas <philipp@jardas.de>
2018-11-09 15:41:13 +0100

    Add signup with GitHub

--- gui/src/components/FontAwesome.js
+++ gui/src/components/FontAwesome.js
@@ -9,9 +9,24 @@ import {
   faShareAlt,
   faSpinner,
   faTrash,
+  faUser,
 } from '@fortawesome/free-solid-svg-icons';
+import { faGoogle, faGithub } from '@fortawesome/free-brands-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-library.add(faChevronLeft, faCheck, faEdit, faExclamationCircle, faExclamationTriangle, faMusic, faShareAlt, faSpinner, faTrash);
+library.add(
+  faChevronLeft,
+  faCheck,
+  faEdit,
+  faExclamationCircle,
+  faExclamationTriangle,
+  faGithub,
+  faGoogle,
+  faMusic,
+  faShareAlt,
+  faSpinner,
+  faTrash,
+  faUser
+);
 
 export default FontAwesomeIcon;
--- gui/src/components/Layout.js
+++ gui/src/components/Layout.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import Helmet from 'react-helmet';
 import { NavLink as RRLink } from 'react-router-dom';
-import { Container, Navbar, NavbarBrand } from 'reactstrap';
+import { Container, Navbar, NavbarBrand, Nav, UncontrolledDropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap';
 import FontAwesome from './FontAwesome';
+import { WithAuth } from '../providers/Auth';
 
-export default function Layout({ title, icon = 'music', back = '/songs', children }) {
+function Layout({ title, icon = 'music', back = '/songs', user, signOut, children }) {
   return (
     <div>
       <Helmet titleTemplate="%s - Songbook" defaultTitle="Songbook">
@@ -16,9 +17,25 @@ export default function Layout({ title, icon = 'music', back = '/songs', childre
             <FontAwesome icon={icon} className="ml-3 mr-2" />
             {title || 'Songbook'}
           </NavbarBrand>
+
+          {user && (
+            <Nav navbar className="ml-auto">
+              <UncontrolledDropdown nav inNavbar>
+                <DropdownToggle nav caret>
+                  <FontAwesome icon="user" className="mr-2" />
+                  {user.label}
+                </DropdownToggle>
+                <DropdownMenu right>
+                  <DropdownItem onClick={signOut}>Sign out</DropdownItem>
+                </DropdownMenu>
+              </UncontrolledDropdown>
+            </Nav>
+          )}
         </Container>
       </Navbar>
       {children}
     </div>
   );
 }
+
+export default props => <WithAuth>{({ user, signOut }) => <Layout {...props} user={user} signOut={signOut} />}</WithAuth>;
