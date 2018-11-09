a8c42eb349bf74229ea2a87c531fd75015b5f637
Philipp Jardas <philipp@jardas.de>
2018-11-09 15:50:24 +0100

    Display user info in nav

--- gui/src/components/Layout.js
+++ gui/src/components/Layout.js
@@ -3,6 +3,7 @@ import Helmet from 'react-helmet';
 import { NavLink as RRLink } from 'react-router-dom';
 import { Container, Navbar, NavbarBrand, Nav, UncontrolledDropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap';
 import FontAwesome from './FontAwesome';
+import UserIcon from './UserIcon';
 import { WithAuth } from '../providers/Auth';
 
 function Layout({ title, icon = 'music', back = '/songs', user, signOut, children }) {
@@ -22,10 +23,12 @@ function Layout({ title, icon = 'music', back = '/songs', user, signOut, childre
             <Nav navbar className="ml-auto">
               <UncontrolledDropdown nav inNavbar>
                 <DropdownToggle nav caret>
-                  <FontAwesome icon="user" className="mr-2" />
-                  {user.label}
+                  {user.photoURL ? <UserIcon user={user} /> : user.label}
                 </DropdownToggle>
                 <DropdownMenu right>
+                  {user.displayName && <DropdownItem>{user.displayName}</DropdownItem>}
+                  {user.email && <DropdownItem>{user.email}</DropdownItem>}
+                  <DropdownItem divider />
                   <DropdownItem onClick={signOut}>Sign out</DropdownItem>
                 </DropdownMenu>
               </UncontrolledDropdown>
