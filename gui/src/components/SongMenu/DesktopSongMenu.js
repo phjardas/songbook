6e849ee5c2859daacb99e638bf9a8ebfe8215ff8
Philipp Jardas <philipp@jardas.de>
2018-12-02 15:38:28 +0100

    Add share action (only on mobile)
    Fixes #5

--- gui/src/components/SongMenu/DesktopSongMenu.js
+++ gui/src/components/SongMenu/DesktopSongMenu.js
@@ -1,21 +1,32 @@
-import { IconButton, Tooltip, withStyles } from '@material-ui/core';
+import { IconButton, withStyles } from '@material-ui/core';
 import { ArrowBack as ArrowBackIcon } from '@material-ui/icons';
 import React from 'react';
 import ButtonLink from '../ButtonLink';
+import NoprintTooltip from './NoprintTooltip';
 import WithMenuItems from './WithMenuItems';
+
+function Action({ Icon, label, onClick }) {
+  return (
+    <NoprintTooltip title={label}>
+      <IconButton onClick={onClick} color="inherit">
+        <Icon />
+      </IconButton>
+    </NoprintTooltip>
+  );
+}
 
 function DesktopSongMenu({ classes, ...props }) {
   const { song } = props;
 
   return (
     <div className={classes.root}>
-      <Tooltip title={`Back to ${song.meta.draft ? 'drafts' : 'songs'}`}>
-        <ButtonLink Component={IconButton} to={song.meta.draft ? '/drafts' : '/songs'}>
+      <NoprintTooltip title={`Back to ${song.meta.draft ? 'drafts' : 'songs'}`}>
+        <ButtonLink color="inherit" Component={IconButton} to={song.meta.draft ? '/drafts' : '/songs'}>
           <ArrowBackIcon />
         </ButtonLink>
-      </Tooltip>
+      </NoprintTooltip>
 
-      <WithMenuItems {...props}>{({ items }) => items.map((Item, i) => <Item key={i} />)}</WithMenuItems>
+      <WithMenuItems {...props}>{({ items }) => items.map((item, i) => <Action {...item} key={i} />)}</WithMenuItems>
     </div>
   );
 }
